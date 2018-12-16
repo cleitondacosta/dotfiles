25f85912e1124e066aeb144a48f651753b8fcd87
cleiton <cleitonjrti@gmail.com>
2018-12-15 22:24:44 -0200

    bashrc: no vi mode and bugfix in PS1; install.sh: improved

--- install.sh
+++ install.sh
@@ -2,11 +2,33 @@
 
 alias errcho=">&2 echo"
 
+function softlink_and_print()
+{
+    SOURCE=$1
+    DESTINATION=$2
+
+    ln -s $SOURCE $DESTINATION
+    [[ $? -eq 0 ]]\
+        && echo "[>] $DESTINATION → $SOURCE"\
+        || errcho "Couldn't link \"$SOURCE\" to \"$DESTINATION\"."
+}
+
+function softlink_force_and_print()
+{
+    SOURCE=$1
+    DESTINATION=$2
+
+    ln -sf $SOURCE $DESTINATION
+    [[ $? -eq 0 ]]\
+        && echo "[>] $DESTINATION → $SOURCE"\
+        || errcho "Couldn't link \"$SOURCE\" to \"$DESTINATION\"."
+}
+
 function install_dotfile()
 {
     SOURCE=$1
     DESTINATION=$2
-    DESTINATION_PARENT_DIR=`dirname $DESTINATION`
+    DESTINATION_PARENT_DIR=$(dirname $DESTINATION)
 
     if [ ! -f $SOURCE ]
     then
@@ -17,7 +39,17 @@ function install_dotfile()
             mkdir -p $DESTINATION_PARENT_DIR
         fi
 
-        ln -si $SOURCE $DESTINATION
+        if [ -e "$DESTINATION" ]
+        then
+            echo -n "\"$DESTINATION\" already exists. Replace it? [y/n] "
+            read ANSWER
+
+            case $ANSWER in
+                y|Y) softlink_force_and_print $SOURCE $DESTINATION
+            esac
+        else
+            softlink_and_print $SOURCE $DESTINATION
+        fi
     fi
 }
 
@@ -25,30 +57,31 @@ function install_directory()
 {
     SOURCE=$1
     DESTINATION=$2
-    DESTINATION_PARENT_DIR=`dirname $DESTINATION`
+    DESTINATION_PARENT_DIR=$(dirname $DESTINATION)
 
-    if [ -d $DESTINATION ]
+    if [ ! -d $SOURCE ]
     then
-        echo 
-        echo "\"$DESTINATION\" already exists."
+        errcho "No such directory: $SOURCE"
+        echo "Ignored ..."
+    elif [ -d $DESTINATION ]
+    then
+        echo
+        echo "Directory \"$DESTINATION\" already exists."
         echo "That means that \"$SOURCE\" would be at:"
         echo "\"$DESTINATION/${SOURCE##*/}\""
         echo "Ignored ..."
         echo
-    elif [ ! -d $SOURCE ]
-    then
-        errcho "No such directory: $SOURCE"
     else
         if [ ! -d $DESTINATION_PARENT_DIR ]
         then
             mkdir -p $DESTINATION_PARENT_DIR
         fi
 
-        ln -si $SOURCE $DESTINATION
+        softlink_and_print $SOURCE $DESTINATION
     fi
 }
 
-DOTFILES_DIR=`pwd`/dotfiles
+DOTFILES_DIR=$(pwd)/dotfiles
 
 if [ ! -d "$DOTFILES_DIR" ]
 then
@@ -58,7 +91,6 @@ then
     exit 1
 fi
 
-install_dotfile "$DOTFILES_DIR/inputrc" ~/.inputrc
 install_dotfile "$DOTFILES_DIR/Xdefaults" ~/.Xdefaults
 install_dotfile "$DOTFILES_DIR/i3blocks.conf" ~/.i3blocks.conf
 install_dotfile "$DOTFILES_DIR/i3config" ~/.config/i3/config
