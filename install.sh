7e01a32d0df04c0c8890a325db2019fde5c373a1
cleiton <cleitonjrti@gmail.com>
2018-12-26 23:56:44 -0200

    Adding experimental feature to automate configuration

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 
 alias errcho=">&2 echo"
 
+function install_packages()
+{
+    PACKAGES_TO_INSTALL=$1
+
+    echo "$PACKAGES_TO_INSTALL"
+    echo
+    echo "Install these packages through pacman? [y/n]"
+    read ANSWER
+
+    case $ANSWER in
+        y|Y) sudo pacman --noconfirm -S $PACKAGES_TO_INSTALL ;;
+        *) echo "Ignored ..." ;;
+    esac
+
+    echo
+}
+
 function softlink_and_print()
 {
     SOURCE=$1
@@ -24,7 +41,7 @@ function softlink_force_and_print()
         || errcho "Couldn't link \"$SOURCE\" to \"$DESTINATION\"."
 }
 
-function install_dotfile()
+function install_a_dotfile()
 {
     SOURCE=$1
     DESTINATION=$2
@@ -53,7 +70,7 @@ function install_dotfile()
     fi
 }
 
-function install_directory()
+function install_a_directory()
 {
     SOURCE=$1
     DESTINATION=$2
@@ -61,16 +78,10 @@ function install_directory()
 
     if [ ! -d $SOURCE ]
     then
-        errcho "No such directory: $SOURCE"
-        echo "Ignored ..."
-    elif [ -d $DESTINATION ]
+        errcho "No such directory: \"$SOURCE\". Ignored ..."
+    elif [ -e $DESTINATION ]
     then
-        echo
-        echo "Directory \"$DESTINATION\" already exists."
-        echo "That means that \"$SOURCE\" would be at:"
-        echo "\"$DESTINATION/${SOURCE##*/}\""
-        echo "Ignored ..."
-        echo
+        echo "\"$DESTINATION\" already exists. Ignored ..."
     else
         if [ ! -d $DESTINATION_PARENT_DIR ]
         then
@@ -81,23 +92,48 @@ function install_directory()
     fi
 }
 
+function recomendations()
+{
+    echo
+    echo "Now that you're set, maybe you want to: "
+    echo "  Install a display manager (with greeter?)?"
+    echo "  Install aurman?"
+    echo "  Install vim-plug?"
+    echo "  Install light? (xbacklight may not work)"
+    echo "  Change ~/.config/users-dirs.dirs?"
+}
+
 DOTFILES_DIR=$(pwd)/dotfiles
+PACKAGE_LIST_FILE="./to_install_pacman.txt"
 
 if [ ! -d "$DOTFILES_DIR" ]
 then
-    errcho "No such directory: $DOTFILES_DIR."
+    errcho "No such directory: \"$DOTFILES_DIR\""
     errcho "See the readme."
 
     exit 1
 fi
 
-install_dotfile "$DOTFILES_DIR/Xdefaults" ~/.Xdefaults
-install_dotfile "$DOTFILES_DIR/i3blocks.conf" ~/.i3blocks.conf
-install_dotfile "$DOTFILES_DIR/i3config" ~/.config/i3/config
-install_dotfile "$DOTFILES_DIR/nvimrc" ~/.config/nvim/init.vim
-install_dotfile "$DOTFILES_DIR/ranger.conf" ~/.config/ranger/rc.conf
-install_dotfile "$DOTFILES_DIR/bashrc" ~/.bashrc
-install_dotfile "$DOTFILES_DIR/dunstrc" ~/.config/dunst/dunstrc
-install_dotfile "$DOTFILES_DIR/qutebrowser_config.py" ~/.config/qutebrowser/config.py
-install_directory "$DOTFILES_DIR/scripts" ~/.scripts
-install_directory "$DOTFILES_DIR/rofi-themes" ~/.config/rofi
+if [ ! -f "$PACKAGE_LIST_FILE" ]
+then
+    errcho "No such file: \"$PACKAGE_LIST_FILE\""
+    errcho "See the readme."
+
+    exit 1
+fi
+
+install_packages "$(tr '\n' ' ' < $PACKAGE_LIST_FILE)"
+
+install_a_dotfile "$DOTFILES_DIR/Xdefaults" ~/.Xdefaults
+install_a_dotfile "$DOTFILES_DIR/i3blocks.conf" ~/.i3blocks.conf
+install_a_dotfile "$DOTFILES_DIR/i3config" ~/.config/i3/config
+install_a_dotfile "$DOTFILES_DIR/nvimrc" ~/.config/nvim/init.vim
+install_a_dotfile "$DOTFILES_DIR/ranger.conf" ~/.config/ranger/rc.conf
+install_a_dotfile "$DOTFILES_DIR/bashrc" ~/.bashrc
+install_a_dotfile "$DOTFILES_DIR/dunstrc" ~/.config/dunst/dunstrc
+install_a_dotfile "$DOTFILES_DIR/qutebrowser_config.py" \
+                  ~/.config/qutebrowser/config.py
+install_a_directory "$DOTFILES_DIR/scripts" ~/.scripts
+install_a_directory "$DOTFILES_DIR/rofi-themes" ~/.config/rofi
+
+recomendations
