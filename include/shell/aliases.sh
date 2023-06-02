3ecc06a55116401255a9ed38a82a64970dd535d6
cleiton <cleitonjrti@gmail.com>
2023-06-02 20:38:51 -0300

    Script "screen-record": Improved. Now it detect if there is more than one monitor, and asks the user which one is to record.

--- include/shell/aliases.sh
+++ include/shell/aliases.sh
@@ -1,5 +1,4 @@
 alias find="fd"
-alias grep="rg"
 alias keycodes="xmodmap -pke"
 alias c='cd "$(fd --type d --hidden --follow --exclude .git | fzf)"'
 alias p='cd "$(ls ~/code/*/project/* -d | fzf)"'
@@ -10,4 +9,8 @@ alias cdi="cd ~/image"
 alias cdv="cd ~/video"
 alias cdr="cd ~/.myrice"
 alias cdm="cd ~/music"
-
+alias update_pacman_mirrorlist="sudo reflector --sort rate --save /etc/pacman.d/mirrorlist -c br && sudo pacman -Sy"
+alias list_installed_fonts="fc-list"
+alias get_sound_volume="pactl get-sink-volume @DEFAULT_SINK@ | grep Volume | awk '{print $5}' | tr '%' ' '"
+alias increase_sound_volume="pactl set-sink-volume @DEFAULT_SINK@ +5%"
+alias decrease_sound_volume="pactl set-sink-volume @DEFAULT_SINK@ -5%"
