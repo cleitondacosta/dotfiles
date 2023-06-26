505e8587d192705c785ae9f3ddd586cc64727e02
cleiton <cleitonjrti@gmail.com>
2023-06-25 21:38:39 -0300

    fix: nvim: telescope: ignoring .git directory

--- dotfiles/config/nvim/after/plugin/telescope.lua
+++ dotfiles/config/nvim/after/plugin/telescope.lua
@@ -1,15 +1,17 @@
 local telescope = require('telescope')
-local builtin = require('telescope/builtin')
 
 telescope.setup {
+    defaults = {
+        file_ignore_patterns = { ".git/" },
+    },
     extensions = {
         fzf = {
             fuzzy = true,
             override_generic_sorter = true,
             override_file_sorter = true,
             case_mode = "smart_case",
-        }
-    }
+        },
+    },
 }
 
 telescope.load_extension('fzf')
