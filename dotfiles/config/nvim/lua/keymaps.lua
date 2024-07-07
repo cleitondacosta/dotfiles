c860dd20cc54a08405b1fc6a811d8fe28a35cc28
cleiton <cleitonjrti@gmail.com>
2024-07-07 15:55:57 -0300

    nvim: adding 'Trouble' plugin and settings up the 'Comment' one

--- dotfiles/config/nvim/lua/keymaps.lua
+++ dotfiles/config/nvim/lua/keymaps.lua
@@ -148,6 +148,40 @@ keymaps.on_gitsigngs_attach = function(bufnr)
     map('n', '<leader>hc', gitsigns.toggle_deleted)
 end
 
-vim.keymap.set('n', '<leader>x', utils.toggle_auto_save)
+--Trouble
+keymaps.trouble = {
+    {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+    },
+    {
+       "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+    },
+    {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+    },
+   {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+    },
+}
+
+-- vim.keymap.set('n', '<leader>x', utils.toggle_auto_save)
 
 return keymaps
