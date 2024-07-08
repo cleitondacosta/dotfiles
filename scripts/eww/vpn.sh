ad96e26932fc7eccf3c4181fbdf304ec08d60385
cleiton <cleitonjrti@gmail.com>
2024-07-08 08:10:14 -0300

    eww: added new vpn window

--- scripts/eww/vpn.sh
+++ scripts/eww/vpn.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 OUTPUT="$(nmcli -f type connection show --active | tail +2 | grep tun)"
+isVpnActive="$(echo -n $(eww get isVpnActive))"
 
 if [ -z "$OUTPUT" ]; then
+    eww update "isVpnActive=false"
+    if [ $isVpnActive = "true" ]; then
+        eww close vpn
+        eww close vpn2
+    fi
     echo "󰱟"
 else
+    eww update "isVpnActive=true"
+    if [ $isVpnActive = "false" ]; then
+        eww open vpn
+        eww open vpn2
+    fi
     echo "󰱔"
 fi
 
