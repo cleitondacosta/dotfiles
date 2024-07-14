38206e5436bd1234023783d3b0ee06c07bb25d2c
cleiton <cleitonjrti@gmail.com>
2024-07-14 12:28:05 -0300

    Adding taskwarrior

--- dotfiles/config/fish/aliases.fish
+++ dotfiles/config/fish/aliases.fish
@@ -21,3 +21,5 @@ alias decrease_sound_volume 'pactl set-sink-volume @DEFAULT_SINK@ -5%'
 alias set_background_image 'feh --bg-fill ~/.bg'
 alias n "nvim /tmp/.quick-note-$USER"
 alias bot "btm"
+alias tl 'task list'
+alias ta 'task add'
