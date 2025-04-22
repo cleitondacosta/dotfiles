294bfc8e413d439f2023b5351ca186bcbdd956f9
cleiton <cleitonjrti@gmail.com>
2025-04-22 18:41:41 -0300

    feat(neovim): changes theme to neofusion

---
+++ dotfiles/config/nvim/lua/external-plugins/theme.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "diegoulloao/neofusion.nvim",
+        config = function()
+            vim.o.background = "dark"
+            vim.cmd([[ colorscheme neofusion ]])
+        end,
+    }
+}
