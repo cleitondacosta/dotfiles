bb236158673fefd6a567378f58e2ba35d58d4ec4
cleiton <cleitonjrti@gmail.com>
2025-04-24 21:12:53 -0300

    fix(neovim, oil): fix oil not showing hidden files

--- dotfiles/config/nvim/lua/external-plugins/oil.lua
+++ dotfiles/config/nvim/lua/external-plugins/oil.lua
@@ -3,7 +3,11 @@ return {
         'stevearc/oil.nvim',
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require("oil").setup()
+            require("oil").setup({
+                view_options = {
+                    show_hidden = true,
+                },
+            })
         end
     },
 }
