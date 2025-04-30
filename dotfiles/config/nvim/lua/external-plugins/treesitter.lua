53411f0d92f4e998acb30d41574e5551256a9987
cleiton <cleitonjrti@gmail.com>
2025-04-30 17:54:54 -0300

    feat(neovim): enable treesiter highlight

--- dotfiles/config/nvim/lua/external-plugins/treesitter.lua
+++ dotfiles/config/nvim/lua/external-plugins/treesitter.lua
@@ -4,7 +4,12 @@ return {
         build = ":TSUpdate",
         config = function()
             ---@diagnostic disable-next-line: missing-fields
-            require('nvim-treesitter.configs').setup { auto_install = true, }
+            require('nvim-treesitter.configs').setup {
+                auto_install = true,
+                hightlight = {
+                    enable = true,
+                }
+            }
         end,
     }
 }
