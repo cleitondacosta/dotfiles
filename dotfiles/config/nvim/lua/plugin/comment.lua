51d322e490bd81e1aea165cbf1b3542bd1fcd797
cleiton <cleitonjrti@gmail.com>
2025-04-30 22:24:48 -0300

    feat(neovim): add nvim-ts-context-commentstring plugin to comment JSX properly

--- dotfiles/config/nvim/lua/plugin/comment.lua
+++ dotfiles/config/nvim/lua/plugin/comment.lua
@@ -1,8 +1,38 @@
 return {
     {
         'numToStr/Comment.nvim',
-        config = function ()
-            require('Comment').setup()
+        dependencies = {
+            {
+                'JoosepAlviste/nvim-ts-context-commentstring',
+            }
+        },
+        config = function()
+            require('Comment').setup {
+                ---@diagnostic disable-next-line: assign-type-mismatch
+                post_hook = nil,
+                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+                padding = true,
+                sticky = true,
+                ---@diagnostic disable-next-line: assign-type-mismatch
+                ignore = nil,
+                mappings = {
+                    basic = true,
+                    extra = true,
+                },
+                toggler = {
+                    line = 'gcc',
+                    block = 'gbc',
+                },
+                extra = {
+                    above = 'gcO',
+                    below = 'gco',
+                    eol = 'gcA',
+                },
+                opleader = {
+                    line = 'gc',
+                    block = 'gb',
+                },
+            }
         end
     }
 }
