928414900a138ef477f8d79752c3ba732100207f
cleiton <cleitonjrti@gmail.com>
2025-04-30 22:06:53 -0300

    fix(neovim): fix typo in treesitter config

--- dotfiles/config/nvim/lua/plugin/treesitter.lua
+++ dotfiles/config/nvim/lua/plugin/treesitter.lua
@@ -3,13 +3,12 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
-            ---@diagnostic disable-next-line: missing-fields
             require('nvim-treesitter.configs').setup {
                 auto_install = true,
                 indent = {
                     enable = true,
                 },
-                hightlight = {
+                highlight = {
                     enable = true,
                 },
             }
