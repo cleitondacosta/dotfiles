665d26d304f867b27ea5a5a5a7a536df8efcd9cd
cleiton <cleitonjrti@gmail.com>
2025-05-01 00:17:41 -0300

    fix(neovim): remove trouble plugin

--- dotfiles/config/nvim/lua/keymaps.lua
+++ dotfiles/config/nvim/lua/keymaps.lua
@@ -188,40 +188,6 @@ keymaps.on_gitsigngs_attach = function(bufnr)
     map('n', '<leader>hc', gitsigns.toggle_deleted)
 end
 
---Trouble
-keymaps.trouble = {
-    {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-    },
-    {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-        "<leader>cs",
-        "<cmd>Trouble symbols toggle focus=false<cr>",
-        desc = "Symbols (Trouble)",
-    },
-    {
-        "<leader>cl",
-        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-        desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)",
-    },
-    {
-        "<leader>xQ",
-        "<cmd>Trouble qflist toggle<cr>",
-        desc = "Quickfix List (Trouble)",
-    },
-}
-
 vim.keymap.set('n', '<leader>1', function() harpoonMarks.set_current_at(1) end, {})
 vim.keymap.set('n', '<leader>2', function() harpoonMarks.set_current_at(2) end, {})
 vim.keymap.set('n', '<leader>3', function() harpoonMarks.set_current_at(3) end, {})
