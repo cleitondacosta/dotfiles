b9fa37d1146133bea243f075e43adb5051d1e793
cleiton <cleitonjrti@gmail.com>
2025-05-07 21:02:33 -0300

    feat(nvim): setup nvim-treesitter

--- dotfiles/config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/config/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,16 @@ return {
         build = ":TSUpdate",
         config = function()
             require('nvim-treesitter.configs').setup {
+                ensure_installed = {
+                    'angular', 'bash', 'c', 'c_sharp', 'css', 'dockerfile',
+                    'fish', 'html', 'javascript', 'json', 'lua', 'markdown',
+                    'python', 'query', 'rust', 'scss', 'sql', 'ssh_config',
+                    'toml', 'tsx', 'typescript', 'vim', 'vimdoc', 'xml',
+                    'yaml', 'yuck'
+                },
+                sync_install = false,
+                ignore_install = {},
+                modules = {},
                 auto_install = true,
                 indent = {
                     enable = true,
