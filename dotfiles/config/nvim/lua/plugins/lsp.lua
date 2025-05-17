cbab8b4c1f2fcc7b121da26fccfa035b611dca2d
cleiton <cleitonjrti@gmail.com>
2025-05-17 15:48:09 -0300

    fix(neovim): fix mason-lspconfig breaking change

--- dotfiles/config/nvim/lua/plugins/lsp.lua
+++ dotfiles/config/nvim/lua/plugins/lsp.lua
@@ -12,19 +12,16 @@ return {
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-            vim.diagnostic.config({virtual_text = false})
+            vim.diagnostic.config({ virtual_text = false })
 
             local mason_lspconfig = require 'mason-lspconfig'
 
-            mason_lspconfig.setup {}
-            mason_lspconfig.setup_handlers {
-                function(server_name)
-                    require('lspconfig')[server_name].setup {
-                        capabilities = capabilities,
-                        on_attach = require('keymaps').on_lsp_attach,
-                    }
-                end,
-            }
+            mason_lspconfig.setup()
+
+            vim.lsp.config('*', {
+                capabilities = capabilities,
+                on_attach = require('keymaps').on_lsp_attach,
+            })
         end,
     },
 }
