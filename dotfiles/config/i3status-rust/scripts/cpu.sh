bef81d360e55e520950047748ca294672ac1b507
cleiton <cleitonjrti@gmail.com>
2025-05-18 08:03:06 -0300

    refactor(i3status-rust): simplifies implementation

--- dotfiles/config/i3status-rust/scripts/cpu.sh
+++ dotfiles/config/i3status-rust/scripts/cpu.sh
@@ -5,10 +5,4 @@
 CPU_TEMP="$(sensors -j | jq '."k10temp-pci-00c3"."Tctl"."temp1_input"' | xargs printf "%.0f")"
 CPU_USAGE="$(mpstat 2 1 | tail -1 | awk '{print (100 - $12)""'})"
 
-if [ $CPU_TEMP -ge 70 ] && [ $CPU_TEMP -lt 80 ]; then
-    echo "{\"text\": \"  \\uf2db  $CPU_TEMP° $CPU_USAGE%  \", \"state\": \"warning\"}"
-elif [ $CPU_TEMP -ge 80 ]; then
-    echo "{\"text\": \"  \\uf2db  $CPU_TEMP° $CPU_USAGE%  \", \"state\": \"critical\"}"
-else
-    echo "{\"text\": \"\\uf2db  $CPU_TEMP° $CPU_USAGE%\", \"state\": \"idle\"}"
-fi
+echo "$CPU_TEMP° $CPU_USAGE%"
