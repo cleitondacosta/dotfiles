ba755a63f81c71ae81fa0a2a66c3d6630de5f65f
Cleiton da Costa <cleitonjrti@gmail.com>
2025-11-22 21:46:04 -0300

    feat(neovim): use prettier to format files only when .prettierrc exists

--- dotfiles/config/nvim/lua/plugins/conform.lua
+++ dotfiles/config/nvim/lua/plugins/conform.lua
@@ -3,17 +3,33 @@ return {
         'stevearc/conform.nvim',
         config = function()
             require('conform').setup({
+                formatters_by_ft = {
+                    javascript = { 'prettier' },
+                    typescript = { 'prettier' },
+                    javascriptreact = { 'prettier' },
+                    typescriptreact = { 'prettier' },
+                    json = { 'prettier' },
+                    css = { 'prettier' },
+                    scss = { 'prettier' },
+                    less = { 'prettier' },
+                    html = { 'prettier' },
+                    yaml = { 'prettier' },
+                    markdown = { 'prettier' },
+                },
                 formatters = {
                     prettier = {
-                        require_cwd = true,
-                    },
-                    prettierd = {
-                        require_cwd = true,
+                        condition = function()
+                            local has_prettier_config = vim
+                                .fs
+                                .root(0, { '.prettierrc' }) ~= nil
+
+                            return has_prettier_config
+                        end,
                     },
                 },
                 format_on_save = {
-                    timeout_ms = 1000,
-                    lsp_format = 'fallback',
+                    lsp_fallback = false,
+                    timeout_ms = 500,
                 },
             })
         end,
