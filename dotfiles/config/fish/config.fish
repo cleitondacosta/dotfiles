aefa876b56acf4de3fa08eb173750589563842b3
Cleiton da Costa <cleitonjrti@gmail.com>
2026-01-08 20:41:00 -0300

    chore(fish): add ~/go/bin directory to PATH

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -26,6 +26,7 @@ add_to_path ~/.sass
 add_to_path ~/.flutter/bin
 add_to_path ~/.pub-cache/bin
 add_to_path ~/.dotnet/tools
+add_to_path ~/go/bin
 add_to_path /usr/lib/jvm/java-17-openjdk/bin/
 add_to_path /opt/nvim-linux-x86_64/bin
 
